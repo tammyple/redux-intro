528d16a6393336483b322a0662845981fdcc9dee
Tammy Le <tammyle@192.168.2.6>
2023-03-17 16:38:25 +0700

    initial commit

--- src/service/product/slice.js
+++ src/service/product/slice.js
@@ -1,12 +1,10 @@
-import {
-    createSlice
-} from '@reduxjs/toolkit'
+import { createSlice } from "@reduxjs/toolkit";
 
 export const productSlice = createSlice({
-    name: "product",
-    initialState: [
-        { id: "p1", title: "Product 1", price: 1999 },
-        { id: "p2", title: "Product 2", price: 999 },
-    ],
-    reducers: {}
-})
+  name: "product",
+  initialState: [
+    { id: "p1", title: "Product 1", price: 1000 },
+    { id: "p2", title: "Product 2", price: 500 },
+  ],
+  reducers: {},
+});
